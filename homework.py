b9bfc2d1b86841b6cc29d3c3b0854f17999e67ec
stevengao-ytc <stevengao213@gmail.com>
2026-09-12 14:21:58 +1000

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,16 +1,30 @@
-# In class, we have made a simple turtle program that draws a square.
-# The python file (draw_square.py) for this program is included in the repository for your reference.
-# So the homework is to make a triangle in python turtle module, using the functions we learned in class.
+# In class, we made a 
 
-# TODO: edit this file to make a triangle of any colour or size you like
-# Since we use a new module called turtle, regular python compiler websites (like online-python.com) will not work.
-# Please test your script in turtle supported online python environments, like
-# "https://pythonsandbox.com/turtle"
-# or "https://trinket.io/turtle"
-# or "https://stepindev.com/en/py-playground"
+import random
 
+def count_occurrence(item, my_list):
+    total = 0
+    for i in my_list:
+        if i == item:
+            total += 1 # total = total + 1
+    return total
 
-import turtle
+# mom bought a lot of grocery items
+grocery_item = ["apple", "banana", "broccoli", "milk", "bread"]
+shopping_cart = []
+
+for i in range(100):
+    # throws random items from grocery_item into shopping_cart
+    shopping_cart.append(random.choice(grocery_item))
+
+print(shopping_cart)
+
+print("Mom bought:")
+print(str(count_occurrence("apple", shopping_cart)) + " apples")
+print(str(count_occurrence("banana", shopping_cart)) + " bananas")
+print(str(count_occurrence("broccoli", shopping_cart)) + " broccolis")
+print(str(count_occurrence("milk", shopping_cart)) + " gallons of milk")
+print(str(count_occurrence("bread", shopping_cart)) + " loaves of bread")
 
 
 
